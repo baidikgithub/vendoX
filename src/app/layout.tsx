26060b441cd3406bd5738e59d4436cdefd9e4e0c
Baidik Mazumdar <baidikmazumdar@Baidiks-MacBook-Air.local>
2025-07-12 09:12:53 +0530

    Implementing the layout(header and sider) of the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import MainLayout from '../components/layout/MainLayout';
+import './globals.css';
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
     <body>
-      <AntdRegistry>{children}</AntdRegistry>
+      <AntdRegistry>
+        <MainLayout>
+          {children}
+        </MainLayout>
+      </AntdRegistry>
     </body>
   </html>
 );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Card, Row, Col, Statistic } from 'antd';
+import { ShoppingOutlined, UserOutlined, DollarOutlined, FileTextOutlined } from '@ant-design/icons';
 
-const Home = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ marginBottom: '24px' }}>Dashboard</h1>
+      
+      <Row gutter={[16, 16]}>
+        <Col xs={24} sm={12} lg={6}>
+          <Card>
+            <Statistic
+              title="Total Products"
+              value={1128}
+              prefix={<ShoppingOutlined />}
+              valueStyle={{ color: '#3f8600' }}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} sm={12} lg={6}>
+          <Card>
+            <Statistic
+              title="Total Customers"
+              value={93}
+              prefix={<UserOutlined />}
+              valueStyle={{ color: '#1890ff' }}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} sm={12} lg={6}>
+          <Card>
+            <Statistic
+              title="Total Revenue"
+              value={11280}
+              prefix={<DollarOutlined />}
+              suffix="USD"
+              valueStyle={{ color: '#cf1322' }}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} sm={12} lg={6}>
+          <Card>
+            <Statistic
+              title="Total Orders"
+              value={93}
+              prefix={<FileTextOutlined />}
+              valueStyle={{ color: '#722ed1' }}
+            />
+          </Card>
+        </Col>
+      </Row>
+
+      <Row gutter={[16, 16]} style={{ marginTop: '24px' }}>
+        <Col xs={24} lg={12}>
+          <Card title="Recent Orders" style={{ height: '400px' }}>
+            <p>Recent orders will be displayed here...</p>
+          </Card>
+        </Col>
+        <Col xs={24} lg={12}>
+          <Card title="Top Products" style={{ height: '400px' }}>
+            <p>Top selling products will be displayed here...</p>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Home;
